aeddcedf23f71482a1fb6b6169d28a6009dde3c6
Richard Bairwell <rbairwell@users.noreply.github.com>
2017-08-26 14:52:42 +0100

    Delete test.sh

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-OAUTH_TOKEN="bacd4353ca9a4f771f16dc7d0c16802de87ca025"
-API_URL="https://api.github.com/user/repos?type=all&per_page=100"
-
-HEADERS=`curl -sI -H "Authorization: token ${OAUTH_TOKEN}" -s "${API_URL}" | grep `
-NEXT=$
